f6d4570a6aa1d37e901f0836e665330b2b49df47
AI-HBB <75945892+AI-HBB@users.noreply.github.com>
2021-02-23 22:52:10 -0500

    Update Three swimmers.cpp

--- Contests/Codeforces Round #704 (Div. 2)/Three swimmers.cpp
+++ Contests/Codeforces Round #704 (Div. 2)/Three swimmers.cpp
@@ -5,7 +5,7 @@
 #include <bits/stdc++.h>
 #include <numeric>
 #include <math.h> 
-#include <vector> 
+#include <vector>
 #define forn(i, n) for (int i = 0; i < int(n); i++)
 using namespace std;
 int main() {
