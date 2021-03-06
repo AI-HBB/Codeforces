75a4e6d017dfc7b6ffa7842f2e60ee440b432303
AI-HBB <75945892+AI-HBB@users.noreply.github.com>
2021-03-06 12:47:23 -0500

    Update B. Planet Lapituletti.cpp

--- Contests/Codeforces Round #705 (Div. 2)/B. Planet Lapituletti.cpp
+++ Contests/Codeforces Round #705 (Div. 2)/B. Planet Lapituletti.cpp
@@ -12,29 +12,29 @@ int main(){
 //freopen("moobuzz.in", "r", stdin);
 //freopen("moobuzz.out", "w", stdout);
   int check[10]={0,1,5,-1,-1,2,-1,-1,8,-1};
-	int t; 
+  int t; 
   cin >> t;
-	while(t--){
-		int h,m;
-		cin >> h >> m;
-		char a[6];
-		cin >> a;
-		int num1,num2;
-		num1=(a[0]-'0')*10+a[1]-'0';
-		num2=(a[3]-'0')*10+a[4]-'0';
-		int mirclk[2];
-		for(int j=0; j<10000; j++){
-			mirclk[1]=(num2+j)%m;
-			mirclk[0]=(num1+(num2+j)/m)%h;
+     while(t--){
+	int h,m;
+	cin >> h >> m;
+	char a[6];
+	cin >> a;
+	int num1,num2;
+	num1=(a[0]-'0')*10+a[1]-'0';
+	num2=(a[3]-'0')*10+a[4]-'0';
+	int mirclk[2];
+	 for(int j=0; j<10000; j++){
+	  mirclk[1]=(num2+j)%m;
+	  mirclk[0]=(num1+(num2+j)/m)%h;
 			
-			int n1=mirclk[0]/10, n2=mirclk[0]%10, n3=mirclk[1]/10, n4=mirclk[1]%10;
-			if(check[n1]==-1||check[n2]==-1||check[n3]==-1||check[n4]==-1) continue;
+	   int n1=mirclk[0]/10, n2=mirclk[0]%10, n3=mirclk[1]/10, n4=mirclk[1]%10;
+	    if(check[n1]==-1||check[n2]==-1||check[n3]==-1||check[n4]==-1) continue;
 			
-			if(check[n1]+check[n2]*10<m && check[n3]+check[n4]*10<h){
-				cout << n1 << n2 << ":" << n3 << n4 << endl;
-				break;
-			}
-		}
+	    if(check[n1]+check[n2]*10<m && check[n3]+check[n4]*10<h){
+		cout << n1 << n2 << ":" << n3 << n4 << endl;
+		break;
+	  }
 	}
-}
+     }
+   }
 // A very good logic problem. I was not able to do it and got the idea from another person
